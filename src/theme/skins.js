dc0ebe6f6c6858315fd399f6f444966c7ad8e085
Trey Hoover <trey.hoover@curiositystream.com>
2018-02-15 15:43:12 -0500

    Reduce midtones from palettes

--- src/theme/skins.js
+++ src/theme/skins.js
@@ -1,12 +1,6 @@
 const black = [
   "#ffffff",
   "#999999",
-  "#8c8c8c",
-  "#808080",
-  "#404040",
-  "#333333",
-  "#262626",
-  "#1a1a1a",
   "#0d0d0d",
   "#000000",
 ];
@@ -14,12 +8,6 @@ const black = [
 const gray = [
   "#fafafa",
   "#f5f5f5",
-  "#eeeeee",
-  "#e0e0e0",
-  "#bdbdbd",
-  "#9e9e9e",
-  "#757575",
-  "#737373",
   "#595959",
   "#383838",
 ];
@@ -27,12 +15,6 @@ const gray = [
 const white = [
   "#ffffff",
   "#f2f2f2",
-  "#e6e6e6",
-  "#d9d9d9",
-  "#bfbfbf",
-  "#757575",
-  "#616161",
-  "#424242",
   "#212121",
   "#000000",
 ];
@@ -40,12 +22,6 @@ const white = [
 const red = [
   "#ffebee",
   "#ffcdd2",
-  "#ef9a9a",
-  "#e57373",
-  "#ef5350",
-  "#f44336",
-  "#e53935",
-  "#d32f2f",
   "#c62828",
   "#b71c1c",
 ];
@@ -53,12 +29,6 @@ const red = [
 const orange = [
   "#fff3e0",
   "#ffe0b2",
-  "#ffcc80",
-  "#ffb74d",
-  "#ffa726",
-  "#ff9800",
-  "#fb8c00",
-  "#f57c00",
   "#ef6c00",
   "#e65100",
 ];
@@ -66,12 +36,6 @@ const orange = [
 const yellow = [
   "#fffde7",
   "#fff9c4",
-  "#fff59d",
-  "#fff176",
-  "#ffee58",
-  "#ffeb3b",
-  "#fdd835",
-  "#fbc02d",
   "#f9a825",
   "#f57f17",
 ];
@@ -79,12 +43,6 @@ const yellow = [
 const green = [
   '#e8f5e9',
   '#c8e6c9',
-  '#a5d6a7',
-  '#81c784',
-  '#66bb6a',
-  '#4caf50',
-  '#43a047',
-  '#388e3c',
   '#2e7d32',
   '#1b5e20',
 ];
@@ -92,12 +50,6 @@ const green = [
 const blue = [
   '#e3f2fd',
   '#bbdefb',
-  '#90caf9',
-  '#64b5f6',
-  '#42a5f5',
-  '#2196f3',
-  '#1e88e5',
-  '#1976d2',
   '#1565c0',
   '#0d47a1',
 ];
